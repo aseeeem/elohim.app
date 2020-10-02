62258765e4e1defcd9bf64b17bd72d6473e10a65
Asim Shamim <asim@asimshamim.com>
2020-10-01 23:51:38 -0400

    ok hopefully not purging too much CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  purge: [
-    './src/**/*.js',
-  ],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true
